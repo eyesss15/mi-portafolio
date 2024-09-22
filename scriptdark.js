404c78d2bed1dd83f7e3b8fff752ade3aeb60d46
eyesss15 <96083702+eyesss15@users.noreply.github.com>
2024-09-22 23:13:22 +0200

    Update scriptdark.js

--- scriptdark.js
+++ scriptdark.js
@@ -1,23 +1,24 @@
-const toggleButton = document.getElementById('toggleDarkMode');
-const body = document.body;
+document.addEventListener('DOMContentLoaded', () => {
+    const toggleButton = document.getElementById('toggleDarkMode');
+    const body = document.body;
 
-// Verifica si hay un modo guardado en localStorage
-if (localStorage.getItem('dark-mode') === 'enabled') {
-    body.classList.add('dark-mode');
-    toggleButton.innerText = 'Desactivar modo oscuro';
-}
-
-// Agrega un evento al botón
-toggleButton.addEventListener('click', () => {
-    body.classList.toggle('dark-mode');
-
-    // Cambia el texto del botón
-    if (body.classList.contains('dark-mode')) {
+    // Verifica si hay un modo guardado en localStorage
+    if (localStorage.getItem('dark-mode') === 'enabled') {
+        body.classList.add('dark-mode');
         toggleButton.innerText = 'Desactivar modo oscuro';
-        localStorage.setItem('dark-mode', 'enabled'); // Guarda el modo oscuro
-    } else {
-        toggleButton.innerText = 'Activar modo oscuro';
-        localStorage.removeItem('dark-mode'); // Elimina el modo oscuro
     }
-});
 
+    // Agrega un evento al botón
+    toggleButton.addEventListener('click', () => {
+        body.classList.toggle('dark-mode');
+
+        // Cambia el texto del botón
+        if (body.classList.contains('dark-mode')) {
+            toggleButton.innerText = 'Desactivar modo oscuro';
+            localStorage.setItem('dark-mode', 'enabled'); // Guarda el modo oscuro
+        } else {
+            toggleButton.innerText = 'Activar modo oscuro';
+            localStorage.removeItem('dark-mode'); // Elimina el modo oscuro
+        }
+    });
+});
