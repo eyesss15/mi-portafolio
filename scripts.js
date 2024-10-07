c8f6d45cfb76289d84814cbf206322d65e857953
eyesss15 <96083702+eyesss15@users.noreply.github.com>
2024-10-07 20:14:49 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,10 +1,12 @@
-// Simula una carga durante 4 segundos y luego muestra el contenido
+// Simula una carga durante 4 segundos y luego muestra el contenido y el nav
 window.addEventListener("load", function() {
     setTimeout(function() {
         document.getElementById("loading-screen").style.display = "none";
         document.getElementById("content").style.display = "block";
+        document.querySelector('nav').style.display = 'block'; // Muestra el nav
     }, 4000); // 4 segundos de carga
 });
+
 
 document.querySelector('.comment-form').addEventListener('submit', function(event) {
     event.preventDefault();
